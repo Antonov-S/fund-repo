204de6e96d347fbf3f2ba00ea6d27d343eb2e77b
Antonov-S <65724927+Antonov-S@users.noreply.github.com>
2020-07-31 22:03:57 +0300

    Text Processing - Exercise

---
+++ source/Text Processing - Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Console.WriteLine(CharMultiplaer(input[0], input[1]));
+            
+        }
+
+        static int CharMultiplaer(string first, string second)
+        {
+            int sum = 0;
+            string longest = String.Empty;
+            string shortest = String.Empty;
+
+            if (first.Length > second.Length)
+            {
+                longest = first;
+                shortest = second;
+            }
+            else
+            {
+                longest = second;
+                shortest = first;
+            }
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                sum += shortest[i] * longest[i];
+            }
+
+            for (int i = shortest.Length; i < longest.Length; i++)
+            {
+                sum += longest[i];
+            }
+
+
+            return sum;
+        }
+    }
+}
