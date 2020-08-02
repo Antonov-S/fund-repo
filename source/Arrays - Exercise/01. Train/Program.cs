2f5c9a5e9a969a8b1a3f34acf8c11c76d3aa3854
Antonov-S <65724927+Antonov-S@users.noreply.github.com>
2020-08-02 22:56:50 +0300

    Arrays - Exercise

---
+++ source/Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int[] train = new int[num];
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                train[i] += input;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                sum += train[i];
+            }
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                Console.Write(train[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
