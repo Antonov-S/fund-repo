7fc176914972e76405773642c43baa43f864573a
Antonov-S <anto1nov1@gmail.com>
2020-08-03 12:47:18 +0300

    Text Processing - Lab

---
+++ source/Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+
+            int startIndex = text.IndexOf(word);
+
+            while (startIndex >= 0)
+            {
+                text = text.Remove(startIndex, word.Length);
+                startIndex = text.IndexOf(word);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
