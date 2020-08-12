df7a3a917fc4a9397ba8599ac138c729220e340f
Antonov-S <anto1nov1@gmail.com>
2020-08-12 18:28:28 +0300

    My final exam again

---
+++ source/My final exam again/01. World Tour_Problem Description/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace _01._World_Tour_Problem_Description
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStops = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Travel")
+            {
+                string[] tokens = command.Split(":");
+                if (command.Contains("Add Stop"))
+                {
+                    int index = int.Parse(tokens[1]);
+                    string newStringToAdd = tokens[2];
+
+                    if (index >= 0 && index <= inputStops.Length - 1)
+                    {
+                        inputStops.Insert(index, newStringToAdd);
+                        Console.WriteLine(inputStops);
+                    }
+                }
+
+                if (command.Contains("Remove Stop"))
+                {
+                    int startIndex = int.Parse(tokens[1]);
+                    int endIndex = int.Parse(tokens[2]);
+
+                    if ((startIndex >= 0 && startIndex <= inputStops.Length - 1) && (endIndex >= 0 && endIndex <= inputStops.Length - 1))
+                    {
+                        inputStops.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+                    Console.WriteLine(inputStops);
+
+                }
+
+                if (command.Contains("Switch"))
+                {
+                    string oldString = tokens[1];
+                    string newString = tokens[2];
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(inputStops);
+                    sb.Replace(oldString, newString);
+                    inputStops = sb.ToString();
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {inputStops}");
+        }
+    }
+}
---
+++ source/My final exam again/02. Destination Mapper_Problem Description/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper_Problem_Description
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=]|[/]{1})([A-Z][a-zA-Z]{3,})\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            List<string> validLocations = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                validLocations.Add(match.Groups[2].Value);
+            }
+
+            int travelPoints = 0;
+            for (int i = 0; i < validLocations.Count; i++)
+            {
+                
+                    travelPoints += validLocations[i].Length;                
+            }
+
+            Console.WriteLine($"Destinations: {String.Join(", ", validLocations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+
+            
+        }
+    }
+}
