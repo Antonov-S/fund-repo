408454b39ffcb36d02d065806009238a2aa2c52d
Antonov-S <anto1nov1@gmail.com>
2020-08-14 23:15:20 +0300

    Programming Fundamentals Final Exam Retake - 13 December 2019

---
+++ source/Programming Fundamentals Final Exam Retake - 13 December 2019/02. Boss Rush/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Boss_Rush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var regex = new Regex(@"[|](?<name>[A-Z]+)[|]:[#](?<title>[A-Za-z]+ [A-Za-z]+)[#]");
+                var match = regex.Match(input);
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string title = match.Groups["title"].Value;
+
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armour: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
---
+++ source/Programming Fundamentals Final Exam Retake - 13 December 2019/03. Hero Recruitment/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Hero_Recruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> heros = new Dictionary<string, List<string>>();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string currentHeroName = tokens[1];
+                if (command == "Enroll")
+                {
+
+                    if (heros.ContainsKey(currentHeroName))
+                    {
+                        Console.WriteLine($"{currentHeroName} is already enrolled.");
+                    }
+                    else
+                    {
+                        heros[currentHeroName] = new List<string>();
+                    }
+                }
+                else if (command == "Learn")
+                {
+                    string spell = tokens[2];
+                    if (!heros.ContainsKey(currentHeroName))
+                    {
+                        Console.WriteLine($"{currentHeroName} doesn't exist.");
+                    }
+                    else if (heros[currentHeroName].Contains(spell))
+                    {
+                        Console.WriteLine($"{currentHeroName} has already learnt {spell}.");
+                    }
+                    else
+                    {
+                        heros[currentHeroName].Add(spell);
+
+                    }
+                }
+                else if (command == "Unlearn")
+                {
+                    string spell = tokens[2];
+                    if (!heros.ContainsKey(currentHeroName))
+                    {
+                        Console.WriteLine($"{currentHeroName} doesn't exist.");
+                    }
+                    else if (!heros[currentHeroName].Contains(spell))
+                    {
+                        Console.WriteLine($"{currentHeroName} doesn't know {spell}.");
+                    }
+                    else
+                    {
+                        heros[currentHeroName].Remove(spell);
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Heroes:");
+            foreach (var item in heros.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                Console.Write($"== {item.Key}:");
+
+                int count = 0;
+                foreach (var kvp in item.Value)
+                {
+                    count++;
+                    if (count == item.Value.Count)
+                    {
+                        Console.Write($" {kvp}");
+                    }
+                    else
+                    {
+                        Console.Write($" {kvp},");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
