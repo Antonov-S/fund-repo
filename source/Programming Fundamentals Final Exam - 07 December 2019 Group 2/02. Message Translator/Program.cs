c6168265ced61893048ad767bf0dddb22d62950f
Antonov-S <anto1nov1@gmail.com>
2020-08-18 22:04:59 +0300

    Programming Fundamentals Final Exam - 07 December 2019 Group 2
    the solutions of the second and third problem

--- source/Programming Fundamentals Final Exam - 07 December 2019 Group 2/02. Message Translator/Program.cs
+++ source/Programming Fundamentals Final Exam - 07 December 2019 Group 2/02. Message Translator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _02._Message_Translator
 {
@@ -6,7 +8,35 @@ namespace _02._Message_Translator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"[!]([A-Z][a-z]{3,})[!]:[\[]([A-Za-z]{8,})[\]]");
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+                
+                if (!match.Success)
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+                else
+                {
+                    string command = match.Groups[1].Value;
+                    string message = match.Groups[2].Value;
+                    Console.Write(command + ":" + " ");
+
+                    foreach (var ch in message)
+                    {
+                        int result = ch;
+                        Console.Write(result + " ");
+                    }
+                    Console.WriteLine();
+
+                }
+
+            }
+            
         }
     }
 }
---
+++ source/Programming Fundamentals Final Exam - 07 December 2019 Group 2/03. Nikulden’s meals/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _03._Nikulden_s_meals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> guests = new Dictionary<string, List<string>>();
+            byte unlikedMealsCounter = 0;
+
+            while (input != "Stop")
+            {
+                string[] splited = input.Split("-", StringSplitOptions.RemoveEmptyEntries);
+                string command = splited[0];
+                string guestName = splited[1];
+                string mealName = splited[2];
+
+                if (command == "Like")
+                {
+                    if (!guests.ContainsKey(guestName))
+                    {
+                        guests.Add(guestName, new List<string>() { mealName });
+                    }
+                    else if (!guests[guestName].Contains(mealName))
+                    {
+                        guests[guestName].Add(mealName);
+                    }
+                    
+                }
+                else if (command == "Unlike")
+                {
+                    if (!guests.ContainsKey(guestName))
+                    {
+                        Console.WriteLine($"{guestName} is not at the party.");
+                    }
+                    else if (!guests[guestName].Contains(mealName))
+                    {
+                        Console.WriteLine($"{guestName} doesn't have the {mealName} in his/her collection.");
+                    }
+                    else
+                    {
+                        guests[guestName].Remove(mealName);
+                        unlikedMealsCounter++;
+                        Console.WriteLine($"{guestName} doesn't like the {mealName}.");
+                    }
+                }
+
+                
+                input = Console.ReadLine();
+            }
+
+            foreach (var kvp in guests.OrderByDescending(kvp => kvp.Value.Count).ThenBy(kvp => kvp.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+            }
+
+            Console.WriteLine($"Unliked meals: {unlikedMealsCounter}");
+        }
+    }
+}
